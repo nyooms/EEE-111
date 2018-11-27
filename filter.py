a9e4f0321212b8a2dd14117edc8f7b461213cebb
nyooms <45359460+nyooms@users.noreply.github.com>
2018-11-27 10:07:10 +0800

    Add files via upload

--- filter.py
+++ filter.py
@@ -12,12 +12,12 @@ def put_unique(uni,fc,x,y,w,h):
     face_width = w
     face_height = h
 
-    uni = cv2.resize(uni,(int(face_width*1),int(face_height*1.9)))
-    for i in range(int(face_height*1.9)):
-        for j in range(int(face_width*1)):
+    uni = cv2.resize(uni,(int(face_width*1.4),int(face_height*2)))
+    for i in range(int(face_height*2)):
+        for j in range(int(face_width*1.4)):
             for k in range(3):
                 if uni[i][j][k]<235:
-                    fc[y+i-int(0.375*h)-1][x+j-int(0.25*w)][k] = uni[i][j][k]
+                    fc[y+i-int(0.350*h)-1][x+j-int(0.25*w)][k] = uni[i][j][k]
     return fc
 
 while True:
@@ -29,10 +29,12 @@ while True:
     face_loc = face_recognition.face_locations(rgb_frame)
 
     for (x, y, w, h) in face_loc:
+        ny = y - h
+        nw = w - x
         #Draw box around face
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #cv2.rectangle(frame, (h, x), (y, w), (0, 255, 0), 2)
 
-        frame = put_unique(unique,frame,x,y,w,h)
+        frame = put_unique(unique,frame,h,x,ny,nw)
 
     cv2.imshow('Unique Salonga Filter', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
